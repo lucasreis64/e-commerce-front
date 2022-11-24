4d361f86725ddd48f6e76bdc2de9926f403f2aed
Matheusfnf <matheusfortunatoaw@gmail.com>
2022-11-23 22:11:18 -0300

    layout mainpage + sidebar

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -1,3 +1,17 @@
+import FootBar from "../FootBar/FootBar";
+import NavBar from "../NavBar/NavBar";
+import { Container, Titulo } from "./CarrinhoStyled";
+
 export default function Carrinho(params) {
-    return
+    return (
+      <>
+        <NavBar />
+        <Container>
+          <Titulo>
+            <h1>Meu carrinho</h1>
+          </Titulo>
+          
+        </Container>
+      </>
+    );
 };
---
+++ src/components/Carrinho/CarrinhoStyled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #514c4c;
+  width: 100%;
+  height: 100%;
+
+
+`;
+
+export const Titulo = styled.div`
+  background-color: #514c4c;
+
+  h1 {
+    background-color: #514c4c;
+    margin: 5%;
+  }
+`;
