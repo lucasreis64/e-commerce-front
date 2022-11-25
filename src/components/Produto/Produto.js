e8f19ae3a310d10ddb3e17aa1de490e3b64a6852
Lucas <lucasreis64@gmail.com>
2022-11-25 16:14:40 -0300

    fix: navbar fixed and global padding

--- src/components/Produto/Produto.js
+++ src/components/Produto/Produto.js
@@ -1,32 +1,48 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { ProdutoPage } from "./ProdutoStyled";
 
-export default function Produto(params) {
-    const [produto, setProduto] = useState(null)
+export default function Produto() {
+    const [produto, setProduto] = useState(null);
+    const { id } = useParams();
 
+    useEffect(() => {
+        getProduto();
+    }, []);
 
-    
     async function getProduto() {
         try {
-            const productGet = await axios.get(`https://e-commerce-projetao-back.onrender.com/products?category=${'category'}&id=${'id'}`)
-            setProduto(productGet)
+            const productGet = await axios.get(
+                `https://e-commerce-projetao-back.onrender.com/products?id=${id}`
+                
+            );console.log(productGet.data)
+            setProduto(productGet.data);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     }
 
-    return(
-        <ProdutoPage>
-            <img src={produto.img} alt="" />
-            <h1>{produto.title}</h1>
-            <br/>
-            <h1>{produto.inStock>0?'Produto Disponível':'Produto Indisponível'}</h1>
-            <br/>
-            <h1>{produto.price}</h1>
-            <button>Colocar no carrinho!</button>
-            <p>{produto.description}</p>
-        </ProdutoPage>
-    )
-    
+    return (
+        <>
+            {!produto ? (
+                "Carregando..."
+            ) : (
+                <ProdutoPage>
+                    <img src={produto.img} alt="" />
+                    <h1>{produto.title}</h1>
+                    <br />
+                    <h1>
+                        {produto.inStock > 0
+                            ? "Produto Disponível"
+                            : "Produto Indisponível"}
+                    </h1>
+                    <br />
+                    <h1>{produto.price}</h1>
+                    <button>Colocar no carrinho!</button>
+                    <p>{produto.description}</p>
+                </ProdutoPage>
+            )}
+        </>
+    );
 }
