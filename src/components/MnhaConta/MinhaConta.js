f75c24de9f1dfcea133f5208ec6d0a2dd240007f
Matheusfnf <matheusfortunatoaw@gmail.com>
2022-11-27 14:28:30 -0300

    local navbar and changes

--- src/components/MnhaConta/MinhaConta.js
+++ src/components/MnhaConta/MinhaConta.js
@@ -1,19 +1,22 @@
 import { Categorias, IconeExcluir, MinhaContaStyled } from "./MinhaContaStyled";
 import iconeExcluir from "../../img/Cancel.png";
+import { Link } from "react-router-dom";
 
-export default function MinhaConta({active}){
-     const closeMyAccount = () => {
-       active(false);
-     };
-    return (
-      <MinhaContaStyled myaccount = {active}>
-        <Categorias>
-          <IconeExcluir>
-            <img src={iconeExcluir} onClick ={closeMyAccount} />
-          </IconeExcluir>
-          <p>Minha conta</p>
-          <p>Logout</p>
-        </Categorias>
-      </MinhaContaStyled>
-    );
+export default function MinhaConta({ active }) {
+  const closeMyAccount = () => {
+    active(false);
+  };
+  return (
+    <MinhaContaStyled myaccount={active}>
+      <Categorias>
+        <IconeExcluir>
+          <img src={iconeExcluir} onClick={closeMyAccount} />
+        </IconeExcluir>
+        <p>Minha conta</p>
+        <Link to="/sign-in">
+          <p>Fazer Login</p>
+        </Link>
+      </Categorias>
+    </MinhaContaStyled>
+  );
 }
